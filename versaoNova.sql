ed87fcb1ce1161cd5dd0b5d4cb6fd0b2c1431942
kitten404 <quezia.abrahao@fatec.sp.gov.br>
2018-05-14 19:18:12 -0300

    okokok
    ta ok

--- versaoNova.sql
+++ versaoNova.sql
@@ -46,5 +46,38 @@ BEGIN
 		p.cod=@codp AND f.id=@codf AND b.id=@codb 
 	RETURN
 END
+
+CREATE FUNCTION fn_tabelaProva (@fase VARCHAR(20),@prova VARCHAR(100))
+	RETURNS @tabelaProva table (
+		Codigo INT,
+		Nome VARCHAR(100),
+		Pais VARCHAR(50),
+		Resultado VARCHAR(20)
+	)
+BEGIN
+	DECLARE @codp INT,
+			@codb INT,
+			@codf INT
+		SET @codp = (SELECT cod FROM prova WHERE prova=@prova)
+		SET @codf =(SELECT id FROM fase WHERE nome=@fase)
+
+		INSERT INTO @tabelaProva(Codigo,Nome,Pais,Resultado)
+		SELECT atl.cod,atl.nome,ps.nome,d.resultado FROM desempenho d
+		INNER JOIN prova p
+		ON d.cod_prova=p.cod
+		INNER JOIN fase f
+		ON d.cod_fase=f.id
+		INNER JOIN bateria b
+		ON d.cod_bateria=b.id
+		INNER JOIN atleta atl
+		ON d.cod_atleta=atl.cod
+		INNER JOIN pais ps
+		ON atl.cod_pais=ps.cod
+		WHERE
+		p.cod=@codp AND f.id=@codf
+	RETURN
+END
+
 select * FROM dbo.fn_tabelaDes('Lançamento de Dado / Javelin Throw','1ºCiclo','FINAL') as tabela
 select * FROM dbo.fn_tabelaDes('3000m / 3000m','1ºCiclo','FINAL') as tabelac
+select * FROM dbo.fn_tabelaProva('FINAL','Lançamento de Dado / Javelin Throw') as tabelak
