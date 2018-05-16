9444db5c1b11fed504b54c903dc4c49dedd69dfd
Cake <melissaqgaspar@gmail.com>
2018-05-16 01:35:41 -0300

    Trigger temporária

--- triggers.sql
+++ triggers.sql
@@ -41,3 +41,24 @@ disable trigger t_protegeresultadocs on prova_cs
 disable trigger t_protegeresultadocorrida on prova_corrida
 disable trigger t_protegeatleta on atleta
 disable trigger t_protegepais on pais
+
+
+----Trigger para garantir que não sejam adicionadas notas da fase final antes da hora, trigger temporária
+
+create trigger tr_verificafinal on desempenho 
+for insert
+as
+begin
+	declare @fase int, 
+		    @prova int
+	set @fase = (select cod_fase from inserted)
+	set @prova = (select cod_prova from inserted)
+	if (@fase = 2)
+	begin
+		if ((select count(d.cod_prova) from desempenho d where  d.cod_prova = @prova) < (select count(cod) from atleta where sexo = (select sexo from prova where cod = @prova)))
+		begin
+			rollback transaction
+			raiserror('Não é possivel adicionar notas da fase final nessa prova no momento', 16, 16)
+		end
+	end
+end
