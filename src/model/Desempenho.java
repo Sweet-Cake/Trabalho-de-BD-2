258e86cc383661b768529e5bbe1321d44224353a
Cake <melissaqgaspar@gmail.com>
2018-05-21 13:51:47 -0300

    ok

---
+++ src/model/Desempenho.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Desempenho {
+	private int codAtl;
+	private String nome;
+	private String pais;
+	private String resultado;
+	public int getCodAtl() {
+		return codAtl;
+	}
+	public void setCodAtl(int codAtl) {
+		this.codAtl = codAtl;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getResultado() {
+		return resultado;
+	}
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+	
+}
